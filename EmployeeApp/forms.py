89df11c3436df3a4355835da0b62ac9b1d4662a5
Athul Binu <athul25b@gmail.com>
2024-03-06 02:09:38 +0530

    Manager Dashboard Changes

---
+++ EmployeeApp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from ManagerApp.models import Project, Task, Team, TeamMembers
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['ProjectName', 'Description']
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['Title', 'Description', 'Status', 'Priority', 'StartDate', 'DueDate', 'CompletionDate', 'Comments', 'SkillID']
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = ['TeamName', 'TeamLead', 'TaskID']
+
+class TeamMembersForm(forms.ModelForm):
+    class Meta:
+        model = TeamMembers
+        fields = ['EmployeeID', 'Role', 'TeamID']
