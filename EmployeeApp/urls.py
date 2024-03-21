f6dfd7e810d5dffd2741bbf81e2b4e13532d2c56
Athul Binu <athul25b@gmail.com>
2024-03-21 22:00:35 +0530

    Attendance calendar

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('EmployeeDashboard/',views.EmployeeDashboard),
     path('EmployeeProject/',views.EmployeeProject),
     path('mark_attendance/', views.mark_attendance, name='mark_attendance'),
+    path('leave/', views.leave, name='leave'),
 ]
